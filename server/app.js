94eb89a36ee64e693028d3baf8f7c53364dbffa2
Sudipta Das <sudiptodas033@gmail.com>
2023-09-14 19:52:00 +0530

    first commit

---
+++ server/app.js
@@ -0,0 +1,41 @@
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const express = require("express");
+const app = express();
+
+dotenv.config({ path: "./config.env" });
+
+require("./db/conn");
+// const User = require("./model/userSchema");
+
+app.use(express.json());
+
+app.use(require("./router/auth"));
+
+const PORT = process.env.PORT;
+
+
+//Middleware
+
+// const middleware = (req, res, next) => {
+//     console.log("from middleware");
+//     next();
+// }
+
+
+// app.get("/", (req, res) => {
+//     res.send(`Hello World from server app js`);
+// });
+
+// app.get("/about", (req, res) => {
+//     res.send(`about page`);
+// });
+
+// app.get("/contact", (req, res) => {
+//     res.cookie("Test", "XYZ");
+//     res.send(`contact page`);
+// });
+
+app.listen(PORT, () => {
+    console.log(`server is running at port no ${PORT}`);
+})
